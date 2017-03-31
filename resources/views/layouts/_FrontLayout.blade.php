5cccf3efd19589e9e52679b40a3334d46b82c57f
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-03-31 18:59:57 +0600

    перенесены дефолтные стили

--- resources/views/layouts/_FrontLayout.blade.php
+++ resources/views/layouts/_FrontLayout.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="robots" content="robots.txt">
+
     <title>@yield('title', "HABB - Сообщество геймеров Казахстана")</title>
     <!--title>{{ config('app.name', 'Laravel') }}</title-->
 
