6d5148a29879f4d7a55fc2e9d6290a33914e65ae
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-01 22:13:12 +0600

    хедпер по выводу дампа для дебага добавлен

---
+++ app/Helpers/VarDumper.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 01.04.2017
+ * Time: 22:12
+ */
+
+namespace Helpers;
+
+
+abstract class VarDumper
+{
+    private static $_objects;
+    private static $_output;
+    private static $_depth;
+
+    /**
+     * Converts a variable into a string representation.
+     * This method achieves the similar functionality as var_dump and print_r
+     * but is more robust when handling complex objects such as PRADO controls.
+     * @param $var
+     * @param int $depth
+     * @param bool $highlight
+     * @return string the string representation of the variable
+     * @internal param variable $mixed to be dumped
+     * @internal param maximum $integer depth that the dumper should go into the variable. Defaults to 10.
+     */
+    public static function dump($var,$depth=10,$highlight=false)
+    {
+        self::$_output='';
+        self::$_objects=array();
+        self::$_depth=$depth;
+        self::dumpInternal($var,0);
+        if($highlight)
+        {
+            $result=highlight_string("<?php\n".self::$_output,true);
+            return preg_replace('/&lt;\\?php<br \\/>/','',$result,1);
+        }
+        else
+            return self::$_output;
+    }
+
+    /**
+     * @param $var
+     * @param $level
+     */
+    private static function dumpInternal($var, $level)
+    {
+        switch(gettype($var))
+        {
+            case 'boolean':
+                self::$_output.=$var?'true':'false';
+                break;
+            case 'integer':
+                self::$_output.="$var";
+                break;
+            case 'double':
+                self::$_output.="$var";
+                break;
+            case 'string':
+                self::$_output.="'$var'";
+                break;
+            case 'resource':
+                self::$_output.='{resource}';
+                break;
+            case 'NULL':
+                self::$_output.="null";
+                break;
+            case 'unknown type':
+                self::$_output.='{unknown}';
+                break;
+            case 'array':
+                if(self::$_depth<=$level)
+                    self::$_output.='array(...)';
+                else if(empty($var))
+                    self::$_output.='array()';
+                else
+                {
+                    $keys=array_keys($var);
+                    $spaces=str_repeat(' ',$level*4);
+                    self::$_output.="array\n".$spaces.'(';
+                    foreach($keys as $key)
+                    {
+                        self::$_output.="\n".$spaces."    [$key] => ";
+                        self::$_output.=self::dumpInternal($var[$key],$level+1);
+                    }
+                    self::$_output.="\n".$spaces.')';
+                }
+                break;
+            case 'object':
+                if(($id=array_search($var,self::$_objects,true))!==false)
+                    self::$_output.=get_class($var).'#'.($id+1).'(...)';
+                else if(self::$_depth<=$level)
+                    self::$_output.=get_class($var).'(...)';
+                else
+                {
+                    $id=array_push(self::$_objects,$var);
+                    $className=get_class($var);
+                    $members=(array)$var;
+                    $keys=array_keys($members);
+                    $spaces=str_repeat(' ',$level*4);
+                    self::$_output.="$className#$id\n".$spaces.'(';
+                    foreach($keys as $key)
+                    {
+                        $keyDisplay=strtr(trim($key),array("\0"=>':'));
+                        self::$_output.="\n".$spaces."    [$keyDisplay] => ";
+                        self::$_output.=self::dumpInternal($members[$key],$level+1);
+                    }
+                    self::$_output.="\n".$spaces.')';
+                }
+                break;
+        }
+    }
+
+    public static function VarExport($data) {
+        echo "<pre>";
+        var_export($data);
+        die();
+    }
+}
