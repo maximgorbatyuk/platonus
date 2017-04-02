66adf1db196cc0ba3d83d7b2a8cf599c7edfe9c2
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-02 14:03:31 +0600

    Добавлены свойства в Файл

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,6 +4,19 @@ namespace App\Models;
 
 use LaravelArdent\Ardent\Ardent;
 
+/**
+ * App\Models\File
+ *
+ * @property int $id
+ * @property string $path Путь до загруженного файла uploads/*
+ * @property string $filename Исходное название файла
+ * @property string $uuid Уникальный идентификатор файла
+ * @property string $document_id Связанный документ
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\Models\Document $document
+ * @mixin \Eloquent
+ */
 class File extends Ardent
 {
     public static $relationsData = array(
