b3e7b1d58561713115fd92bec57a34c07092300d
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-04 22:56:38 +0600

    Подключена метрика

--- resources/views/layouts/_FrontLayout.blade.php
+++ resources/views/layouts/_FrontLayout.blade.php
@@ -14,6 +14,7 @@
     <link href="{{ asset('bt/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('thirdparty/fa/font-awesome.min.css') }}" rel="stylesheet" >
     <link href="{{ asset('custom/css/app.css') }}" rel="stylesheet">
+
     @yield('styles')
 
 </head>
@@ -26,6 +27,8 @@
 
     @include('layouts._FrontFooter')
 
+    <noscript><div><img src="https://mc.yandex.ru/watch/43965764" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <script type="text/javascript"  src="{{ asset('thirdparty/yandex/metrica.js') }}"></script>
     <script src="{{ asset('thirdparty/jquery/jquery-3.1.1.slim.min.js') }}"></script>
     <script src="{{ asset('thirdparty/jquery/tether-1.4.0.min.js') }}"></script>
     <script src="{{ asset('bt/js/bootstrap.min.js') }}"></script>
