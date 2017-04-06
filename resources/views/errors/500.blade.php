3f74f1228359ed28899280e9b161765ec3473acb
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-07 00:19:10 +0600

    Модификация страниц ошщибок

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,10 +4,14 @@
 @section('content')
 
     <div class="container">
-        <div class="text-center display-margin">
+        <div class="text-center pt-10 pb-5">
             <h1 class="display-1 text-danger"><strong>Упс!</strong></h1>
             <p class="lead mt-3">
                 Как интересно, во время обработки запроса возникла системная ошибка!
+            </p>
+
+            <p class="text-danger lead mt-3">
+                {{ $exception->getMessage() ?? "" }}
             </p>
         </div>
 
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
     <div class="container">
-        <div class="text-center display-margin">
+        <div class="text-center  pt-10 pb-5">
             <h1 class="display-1">Сайт на реконструкции</h1>
             <p class="lead my-3">
                 Не беспокойтесь, мы скоро вернемся!
