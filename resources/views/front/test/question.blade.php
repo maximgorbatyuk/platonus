d5183c8c5a44b32852ab1d6d1c0b1f9a60db9370
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-09 22:11:38 +0600

    модифицирован шаблон

--- resources/views/front/test/question.blade.php
+++ resources/views/front/test/question.blade.php
@@ -13,11 +13,14 @@
     <div class="container my-2">
         <div class="card card-block">
 
-            <h2>Тестирование </h2>
+            <div class="d-flex w-100 justify-content-between">
+                <h2>Тестирование </h2>
+                <div class="text-muted">5 вопрос из 25</div>
+            </div>
 
             @if (isset($model))
 
-                {{ Form::open(['action' => 'TestController@question']) }}
+                {{ Form::open(['action' => 'TestController@question', 'id' => 'question_form']) }}
 
                 {{ Form::hidden('document_id', $model->document->id) }}
                 {{ Form::hidden('limit', $model->limit) }}
@@ -37,7 +40,7 @@
                 @for($i = 0; $i < count($variants); $i++)
 
                     <div class="form-group">
-                        <label class="form-check-label">
+                        <label class="form-check-label variant">
                             <input
                                     class="form-check-input"
                                     type="radio"
@@ -47,6 +50,22 @@
                     </div>
 
                 @endfor
+
+                <div class="progress my-3">
+                    <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-outline-danger">Закончить</button>
+                    </div>
+                    <div class="col-md-6 text-md-right">
+                        <button type="button" class="btn btn-outline-success">Правильный</button>
+                        <button type="submit" class="btn btn-primary">Далее <i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+
+
 
                 {{Form::close()}}
 
