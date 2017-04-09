feac68c32182d59eac3f117df4840c1665caba64
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-09 10:30:54 +0600

    open graph мета теги

--- resources/views/layouts/_OpenGraphMeta.blade.php
+++ resources/views/layouts/_OpenGraphMeta.blade.php
@@ -1,6 +1,11 @@
 
 <meta property="og:title" content="Пройти тест Platonus и развеселиться"/>
-<meta property="og:description" content="Загрузи и пройди тест без регистрации и смс, побереги себя и своих близких"/>
+<meta property="og:description"
+      content="Портал для подготовки к тестированию по системе Platonus методом пробных тестов.
+      Можно воспользоваться уже загруженным документом, а можно и свой загрузить.
+      Пройди тест, подготовься, получи высший бал на сессии, побереги себя и своих близких."/>
 <meta property="og:image" content="{{ asset('images/logo-vert-white.png')  }}">
-<meta property="og:type" content="article"/>
-<meta property="og:url" content= "{{ url('/') }}" />
+<meta property="og:type" content="website"/>
+<meta property="og:url" content= "@yield('og_url', url('/'))" />
+<meta property="og:site_name" content="Platest.tk">
+<meta property="og:locale" content="ru_RU">
