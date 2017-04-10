76cfd108f3f3f3c01a9528e76e09524fe1fb89cb
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-10 22:06:39 +0600

    Добавлены классы для вывода сообщений

---
+++ app/Helpers/Cheer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 10.04.2017
+ * Time: 21:49
+ */
+
+namespace App\Helpers;
+
+
+
+abstract class Cheer
+{
+    public static function getComment(double $value) {
+        return "Не расстраивайся";
+    }
+}
---
+++ app/Helpers/Swears.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Next
+ * Date: 10.04.2017
+ * Time: 21:46
+ */
+
+namespace App\Helpers;
+
+
+abstract class Swears
+{
+    public static function getComment(double $value) {
+        return "Ну ты и балда";
+    }
+}
