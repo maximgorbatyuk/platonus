29e4447e57ba00beded74cc01121d98a72967841
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-10 22:17:32 +0600

    Обновлен стиль карточки документа

--- resources/views/front/documents/_doc_cards.blade.php
+++ resources/views/front/documents/_doc_cards.blade.php
@@ -3,24 +3,17 @@
         /** @var \App\Models\Document $instance */
     @endphp
 
-    <div class="col-md-4 mt-2">
+    <div class="col-md-4 mt-md-2 my-4">
         <div class="card h-100">
             <div class="card-block mb-0">
                 <a href="{{ url('documents/'.$instance->id) }}" class="h5">
                     #{{ $instance->title }}
                 </a>
 
-                <div class="text-muted">
-                    <dl class="row">
-                        <dt class="col-sm-6">ID</dt>
-                        <dd class="col-sm-6">{{ $instance->id }}</dd>
-
-                        <dt class="col-sm-6">Просмотров</dt>
-                        <dd class="col-sm-6">{{ $instance->views }}</dd>
-
-                        <dt class="col-sm-6">Вопросов</dt>
-                        <dd class="col-sm-6">{{ $instance->question_count }}</dd>
-                    </dl>
+                <div class="text-muted d-flex w-100 justify-content-between">
+                    <div>ID: {{ $instance->id }}</div>
+                    <div>Просмотров: {{ $instance->views }}</div>
+                    <div>Вопросов: {{ $instance->question_count }}</div>
                 </div>
             </div>
             <div class="mt-0 w-100">
