af5821d714ada75750b379523c7fc0abb9379c8c
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-04-10 22:07:16 +0600

    Отредактирован шаблон для карточки документа

--- resources/views/front/documents/_doc_cards.blade.php
+++ resources/views/front/documents/_doc_cards.blade.php
@@ -4,22 +4,27 @@
     @endphp
 
     <div class="col-md-4 mt-2">
-        <div class="card card-block h-100">
-            <a href="{{ url('documents/'.$instance->id) }}" class="h5">
-                #{{ $instance->title }}
-            </a>
+        <div class="card h-100">
+            <div class="card-block mb-0">
+                <a href="{{ url('documents/'.$instance->id) }}" class="h5">
+                    #{{ $instance->title }}
+                </a>
 
-            <div class="text-muted">
-                <dl class="row">
-                    <dt class="col-sm-6">ID</dt>
-                    <dd class="col-sm-6">{{ $instance->id }}</dd>
+                <div class="text-muted">
+                    <dl class="row">
+                        <dt class="col-sm-6">ID</dt>
+                        <dd class="col-sm-6">{{ $instance->id }}</dd>
 
-                    <dt class="col-sm-6">Просмотров</dt>
-                    <dd class="col-sm-6">{{ $instance->views }}</dd>
+                        <dt class="col-sm-6">Просмотров</dt>
+                        <dd class="col-sm-6">{{ $instance->views }}</dd>
 
-                    <dt class="col-sm-6">Вопросов</dt>
-                    <dd class="col-sm-6">{{ $instance->question_count }}</dd>
-                </dl>
+                        <dt class="col-sm-6">Вопросов</dt>
+                        <dd class="col-sm-6">{{ $instance->question_count }}</dd>
+                    </dl>
+                </div>
+            </div>
+            <div class="mt-0 w-100">
+                <a href="#" class="btn-card btn-primary w-100">Открыть</a>
             </div>
         </div>
     </div>
