d3175727e7487add780bd4af6e597437d9b72e9b
Dell-PC <maximgorbatyuk191093@gmail.com>
2017-05-03 21:07:20 +0600

    Добавлен мидл-обработчик

---
+++ app/Http/Middleware/AdminAccess.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAccess extends Authenticate
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if(\Auth::check() && \Auth::user()->is_admin)
+        {
+            return $next($request);
+        }
+
+        if ($request->ajax())
+        {
+            return response('Unauthorized.', 401);
+        }
+        return response('Unauthorized.', 403);
+    }
+}
